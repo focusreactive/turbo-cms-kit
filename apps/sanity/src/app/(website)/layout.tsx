7da21641263589df11499e6826cbf27bc38666c1
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-07-24 09:43:48 +0200

    add conditional rendering of VisualEditing

--- apps/sanity/src/app/(website)/layout.tsx
+++ apps/sanity/src/app/(website)/layout.tsx
@@ -1,40 +1,19 @@
 import { type Metadata } from "next";
-import { revalidatePath, revalidateTag } from "next/cache";
-import { draftMode } from "next/headers";
 import config from "config";
-import { VisualEditing } from "next-sanity";
+
+import VisualEditing from "@/components/VisualEditing";
 
 export const metadata: Metadata = {
   title: `${config.siteName} - Website`,
 };
 
+const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === "true";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      {draftMode().isEnabled && (
-        <VisualEditing
-          refresh={async (payload) => {
-            "use server";
-            if (!draftMode().isEnabled) {
-              console.debug(
-                "Skipped manual refresh because draft mode is not enabled",
-              );
-              return;
-            }
-            if (payload.source === "mutation") {
-              if (payload.document.slug?.current) {
-                const tag = `${payload.document._type}:${payload.document.slug.current}`;
-                console.log("Revalidate slug", tag);
-                await revalidateTag(tag);
-              }
-              console.log("Revalidate tag", payload.document._type);
-              return revalidateTag(payload.document._type);
-            }
-            await revalidatePath("/", "layout");
-          }}
-        />
-      )}
+      {isPreview && <VisualEditing />}
     </>
   );
 }
---
+++ apps/sanity/src/components/VisualEditing/index.tsx
@@ -0,0 +1,31 @@
+import { revalidatePath, revalidateTag } from "next/cache";
+import { draftMode } from "next/headers";
+import { VisualEditing } from "next-sanity";
+
+export default function VisualEditingComponent() {
+  return (
+    draftMode().isEnabled && (
+      <VisualEditing
+        refresh={async (payload) => {
+          "use server";
+          if (!draftMode().isEnabled) {
+            console.debug(
+              "Skipped manual refresh because draft mode is not enabled",
+            );
+            return;
+          }
+          if (payload.source === "mutation") {
+            if (payload.document.slug?.current) {
+              const tag = `${payload.document._type}:${payload.document.slug.current}`;
+              console.log("Revalidate slug", tag);
+              await revalidateTag(tag);
+            }
+            console.log("Revalidate tag", payload.document._type);
+            return revalidateTag(payload.document._type);
+          }
+          await revalidatePath("/", "layout");
+        }}
+      />
+    )
+  );
+}
