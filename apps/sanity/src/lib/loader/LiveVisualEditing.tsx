035e6ec7c5ef96842223f11b8779355b1c8023ca
Eugene Boruhov <eugene@focusreactive.com>
2024-09-09 00:55:28 +0200

    Fix vercel draft mode on production domain

--- apps/sanity/src/lib/loader/LiveVisualEditing.tsx
+++ apps/sanity/src/lib/loader/LiveVisualEditing.tsx
@@ -13,7 +13,7 @@ export default function LiveVisualEditing() {
   useLiveMode({ client: stegaClient });
   useEffect(() => {
     // If not an iframe or a Vercel Preview deployment, turn off Draft Mode
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== "preview" && window === parent) {
+    if (/*process.env.NEXT_PUBLIC_VERCEL_ENV !== "preview" && */window === parent) {
       location.href = "/api/disable-draft";
     }
   }, []);
