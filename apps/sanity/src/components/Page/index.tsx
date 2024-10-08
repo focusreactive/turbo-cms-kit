aeba1c25dbf3ccb3c7c163d1cc7e79444c455bcb
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2024-10-08 18:31:35 +0200

    feature/add show cookie banner param to the page, comment sanity section selector plugin (#19)

--- apps/sanity/src/components/Page/index.tsx
+++ apps/sanity/src/components/Page/index.tsx
@@ -1,17 +1,20 @@
 import { SectionRenderer } from "@/contentSections";
 
+import { CookieBanner } from "@shared/ui";
+
 import type { IPageProps } from "./types";
 
 export default function Page({ data }: IPageProps) {
   if (!data) return null;
 
-  const { sectionsBody } = data;
+  const { sectionsBody, showCookieBanner } = data;
 
   return (
     <div>
       {sectionsBody?.map((section) => (
         <SectionRenderer key={section._key} section={section} />
       ))}
+      {showCookieBanner && <CookieBanner />}
     </div>
   );
 }
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,6 +2,7 @@
 export * from "./components/EmptyBlock";
 export * from "./components/ui/link";
 export * from "./components/ui/image";
+export * from "./components/ui/GenericCarousel";
 export * from "./components/ui/richText";
 export * from "./components/sections/header";
 export * from "./components/sections/footer";
@@ -11,8 +12,8 @@ export * from "./components/sections/linksList";
 export * from "./components/sections/cardsGrid";
 export * from "./components/sections/blog";
 export * from "./components/sections/hero";
-export * from "./components/ui/GenericCarousel";
 export * from "./components/sections/carousels";
+export * from "./components/sections/cookieBanner";
 // end component exports
 
 export { cn } from "./utils";
