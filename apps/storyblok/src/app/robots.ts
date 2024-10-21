d4ec7b0441a345a130e9d61b80046bac1c06fd52
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-10-21 14:29:26 +0200

    feature/prevent AI bots from scrapping website by putting rules to robots.txt

--- apps/storyblok/src/app/robots.ts
+++ apps/storyblok/src/app/robots.ts
@@ -2,10 +2,140 @@ import { type MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "anthropic-ai",
+        disallow: "/",
+      },
+      {
+        userAgent: "Applebot-Extended",
+        disallow: "/",
+      },
+      {
+        userAgent: "Bytespider",
+        disallow: "/",
+      },
+      {
+        userAgent: "CCBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        disallow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Claude-Web",
+        disallow: "/",
+      },
+      {
+        userAgent: "cohere-ai",
+        disallow: "/",
+      },
+      {
+        userAgent: "Diffbot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Meta-ExternalAgent",
+        disallow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        disallow: "/",
+      },
+      {
+        userAgent: "Google-CloudVertexBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Scrapy",
+        disallow: "/",
+      },
+      {
+        userAgent: "Timpibot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Zoominfobot",
+        disallow: "/",
+      },
+      {
+        userAgent: "trovitBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "AhrefsBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "sogou spider",
+        disallow: "/",
+      },
+      {
+        userAgent: "BLEXBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "ShopWiki",
+        disallow: "/",
+      },
+      {
+        userAgent: "MJ12bot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Exabot",
+        disallow: "/",
+      },
+      {
+        userAgent: "CCBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "sistrix",
+        disallow: "/",
+      },
+      {
+        userAgent: "XoviBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Flamingo_SearchEngine",
+        disallow: "/",
+      },
+      {
+        userAgent: "Nutch",
+        disallow: "/",
+      },
+      {
+        userAgent: "ADmantX",
+        disallow: "/",
+      },
+      {
+        userAgent: "grapeshot",
+        allow: "/",
+      },
+      {
+        userAgent: "CarsnipBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${process.env.NEXT_PUBLIC_DOMAIN}/sitemap.xml`,
   };
 }
