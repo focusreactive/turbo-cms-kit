a77c82f88d7a311bf84d235ac4fd20ed0393381f
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-10-30 08:43:31 +0100

    wd

--- packages/ui/components/sections/pricing/index.tsx
+++ packages/ui/components/sections/pricing/index.tsx
@@ -86,7 +86,7 @@ export function Pricing({
                 <div className="mt-4">
                   <Image
                     {...tier.icon}
-                    className="h-12 w-12 text-gray-400"
+                    className="size-12 text-gray-400"
                     fit="contain"
                   />
                 </div>
@@ -115,9 +115,9 @@ export function Pricing({
                 <ul className="mt-6 space-y-4">
                   {tier.features.map((feature) => (
                     <li key={feature} className="flex items-start">
-                      <div className="flex-shrink-0">
+                      <div className="shrink-0">
                         <svg
-                          className="h-6 w-6 text-green-500"
+                          className="size-6 text-green-500"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
--- packages/ui/components/ui/switch/Switch.tsx
+++ packages/ui/components/ui/switch/Switch.tsx
@@ -17,7 +17,7 @@ export const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "bg-gray-400 pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-blue-500",
+        "bg-gray-400 pointer-events-none block size-5 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-blue-500",
       )}
     />
   </SwitchPrimitives.Root>
