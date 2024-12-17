110009ae744b52d1ee438993c7fed7ca33b964f1
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-12-17 15:58:54 +0100

    add getStaticProps generateStaticParams

--- apps/storyblok/src/app/[[...slug]]/page.tsx
+++ apps/storyblok/src/app/[[...slug]]/page.tsx
@@ -5,7 +5,7 @@ import { StoryblokStory } from "@storyblok/react/rsc";
 
 import {
   // checkDraftMode,
-  // fetchAllPages,
+  fetchAllPages,
   fetchStoryBySlug,
   getMetaData,
 } from "@/lib/api";
@@ -27,19 +27,33 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   return await getMetaData(params.slug);
 }
 
+export async function getStaticProps() {
+  const pages = await fetchAllPages();
+
+  const paths = pages
+    .filter((page) => page.is_folder === false)
+    .map((page) => {
+      return {
+        slug: page.slug.split("/"),
+      };
+    });
+
+  return paths;
+  // return [];
+}
+
 export async function generateStaticParams() {
-  return [];
-  // const pages = await fetchAllPages();
+  const pages = await fetchAllPages();
 
-  // const paths = pages
-  //   .filter((page) => page.is_folder === false)
-  //   .map((page) => {
-  //     return {
-  //       slug: page.slug.split("/"),
-  //     };
-  //   });
+  const paths = pages
+    .filter((page) => page.is_folder === false)
+    .map((page) => {
+      return {
+        slug: page.slug.split("/"),
+      };
+    });
 
-  // return paths;
+  return paths;
 }
 
 export default async function Home(props: Props) {
