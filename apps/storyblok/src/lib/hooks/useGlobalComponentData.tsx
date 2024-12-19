172ed98632d1aa196e313acaa55b2c2f40b86dcc
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2024-12-19 10:53:19 +0100

    Feature/fix header footer (#73)
    * add real console
    
    * wd
    
    * fetch global stories for all environments
    
    * remove console.logs

--- apps/storyblok/src/lib/hooks/useGlobalComponentData.tsx
+++ apps/storyblok/src/lib/hooks/useGlobalComponentData.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import type { ISbStoryData } from "@storyblok/react/rsc";
+import type { ISbStoryData, SbBlokData } from "@storyblok/react/rsc";
 
 import { useDataContext } from "@/components/DataContext";
 
 export const useGlobalComponentData = (
-  globalComponent: string,
+  globalComponent: string | SbBlokData,
 ): ISbStoryData | null => {
   const { globalComponentsStories } = useDataContext();
 
