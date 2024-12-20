4b706948f42ab7befd490600c5a23ab792219aab
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-12-20 16:26:39 +0100

    fix build and add error blok to the 3d element

--- packages/ui/components/sections/threeDElement/index.tsx
+++ packages/ui/components/sections/threeDElement/index.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 
+import EmptyBlock from "../../EmptyBlock";
 import DonutModel from "./donut-model";
 import GlobeModel from "./globe-model";
 import KubikRubik from "./kubik-rubik";
@@ -12,9 +13,9 @@ const MODELS_MAP: Record<IThreeDElementProps["model"], FC> = {
 };
 
 export function ThreeDElement({ model }: IThreeDElementProps) {
-  const ModelComponent = MODELS_MAP[model];
+  const ModelComponent = MODELS_MAP[model] as any;
 
-  if (!ModelComponent) return null;
+  if (!ModelComponent) return <EmptyBlock name="3D Element" />;
 
   return (
     <div className="">
